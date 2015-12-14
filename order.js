6b00a0dad002a648ebe189aed8de72c0e9287b26
Jon Wexler <jwexman@gmail.com>
2015-12-13 23:13:44 -0500

    added add and remove functionality

--- order.js
+++ order.js
@@ -1,7 +1,27 @@
-if (Meteor.isClient) {
+Questions = new Mongo.Collection('questions');
 
+if (Meteor.isClient) {
+  Template.main.questions = function(){
+    return Questions.find();
+  }
+  Template.main.events({
+   'click #save': function (e) {
+    var question = $("#question").val();
+    var answer = $("#answer").val();
+    var type = $("#type").val();
+    // e.preventDefault();
+    Questions.insert({question:question, answer:answer, type:type});
+    // alert(question + answer + type + Questions.find());
+    console.log(Questions.find());
+  },
+  'dblclick': function (e) {
+    Questions.remove(this._id);
+    // e.preventDefault();
+    // console.log("You pressed the button");
+  }
+});
 }
 
 if (Meteor.isServer) {
- 
+
 }
